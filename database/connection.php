d02b0ed32c8b5633632f971ad6ca38ab7f97b53a
LarrYdaSnaiL <rafifauzantsany.lubis@gmail.com>
2024-10-02 12:05:49 +0700

    Remove .sql File From Pushing

--- database/connection.php
+++ database/connection.php
@@ -8,9 +8,3 @@ $pass = $env['DB_PASS'];
 $db = $env['DB_NAME'];
 
 $conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
-
-if ($conn) {
-    echo "Connected to the database successfully";
-} else {
-    echo "Error in connecting to the database";
-}
